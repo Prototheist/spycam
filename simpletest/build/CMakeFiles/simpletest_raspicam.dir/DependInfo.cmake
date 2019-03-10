b8c31a16f49240fffc59ae628b4de23bfbfe2c7a
Prototheist <therealprototheist@gmail.com>
2019-03-10 16:08:34 -0400

    Passed compilation on Ubuntu

--- simpletest/build/CMakeFiles/simpletest_raspicam.dir/DependInfo.cmake
+++ simpletest/build/CMakeFiles/simpletest_raspicam.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/pi/Desktop/git_clones/spycam/simpletest/simpletest_raspicam.cpp" "/home/pi/Desktop/git_clones/spycam/simpletest/build/CMakeFiles/simpletest_raspicam.dir/simpletest_raspicam.cpp.o"
+  "/home/cj/Desktop/git_clones/spycam/simpletest/simpletest_raspicam.cpp" "/home/cj/Desktop/git_clones/spycam/simpletest/build/CMakeFiles/simpletest_raspicam.dir/simpletest_raspicam.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
